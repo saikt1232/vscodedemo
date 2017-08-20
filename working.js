f5bcb272d523c47c432c205c0bac14b34c1ad71c
SAIKISHAN SINGH THAKUR <sai.k.tech@gmail.com>
2017-08-19 21:49:18 -0700

    new changes to the files

---
+++ working.js
@@ -0,0 +1,16 @@
+//this file will have all the snippets needed for the js algos
+
+(function(){console.log("this will compile something here!!")})();
+
+//Arrays 
+
+var a = ['sai','ram'];
+var somestr = "Saikishan singhthakur";
+
+console.log(somestr.split('').reverse().join(''));
+
+console.log(a.reverse().join('()'));
+
+// Visual studio if used properly is an amazing tool to build very quick apps neat and handy
+
+//Most important things alwats i
